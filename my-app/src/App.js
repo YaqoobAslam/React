cc7c9337874a18b9ae8e93ff533851dafd761eb8
YaqoobAslam <happyyaqoob@gmail.com>
2020-04-03 01:18:20 -0700

    added dishes component and pass as props to menu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import { Navbar ,NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import './App.css';
+import React, { Component } from "react";
+import Menu from "./components/MenuComponent";
+import { DISHES } from "./shared/dishes";
+import "./App.css";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-
-          </div>
-        </Navbar>
-        <Menu></Menu>
+        <Menu Dishes={this.state.Dishes}></Menu>
       </div>
     );
   }
